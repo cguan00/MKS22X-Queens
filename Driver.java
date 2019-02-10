c9c936916af0ffcb6dca475ab79aab53c75335c4
Christy Guan <cguan00@stuy.edu>
2019-02-09 23:33:35 -0500

    testing addQueen

--- Driver.java
+++ Driver.java
@@ -1,8 +1,9 @@
 public class Driver{
   public static void main(String[] args) {
     QueenBoard board = new QueenBoard(4);
-
-    board.solve();
+    board.addQueen(0,0);
+    System.out.println(board);
+    board.addQueen(1,0);
     System.out.println(board);
   }
 }
